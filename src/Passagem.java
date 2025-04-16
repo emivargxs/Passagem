f6c01e84e8a779fe12f5ecb2b60bd8a83627aa1f
emivargxs <emile.bordin@edu.pucrs.br>
2025-04-16 17:53:20 -0300

    Se não tiver bagagem

--- src/Passagem.java
+++ src/Passagem.java
@@ -11,12 +11,17 @@ public class Passagem {
         this.custoPassagem = custoPassagem;
     }
 
-    public double custoBagagem(int[] pesos) {
+
+    public double custoBagagem(int quantidadeBagagem,int[] pesos) {
         double custo = 0;
+        if(quantidadeBagagem == 0){
+            return custo;
+        } else{
         for (int i = 0; i < pesos.length; i++) {
             custo += pesos[i] * 0.5; // R$0,50 por kg
         }
         return custo;
+    }
     }
 
     public double defineAssento(String assento) {
